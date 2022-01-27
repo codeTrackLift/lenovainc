8686e0474e6fde297db60a151aaa05e3f7fa1b8c
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-01-27 18:07:35 -0500

    scroll effects
    scroll magic effects on cards and buttons

---
+++ index.js
@@ -0,0 +1,143 @@
+// var controllerMainLogo = new ScrollMagic.Controller();
+// var sceneMainLogo = new ScrollMagic.Scene({
+//     triggerElement: '.sceneMainLogo',
+//     offset: 100,
+//     duration: 500
+// })
+// .setClassToggle('.sceneMainLogo', 'show')
+// .addTo(controllerMainLogo);
+
+var controllerBtn1 = new ScrollMagic.Controller();
+var sceneBtn1 = new ScrollMagic.Scene({
+    triggerElement: '.btn1',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn1', 'show')
+.addTo(controllerBtn1);
+
+var controllerBtn2 = new ScrollMagic.Controller();
+var sceneBtn2 = new ScrollMagic.Scene({
+    triggerElement: '.btn2',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn2', 'show')
+.addTo(controllerBtn2);
+
+var controllerBtn3 = new ScrollMagic.Controller();
+var sceneBtn3 = new ScrollMagic.Scene({
+    triggerElement: '.btn3',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn3', 'show')
+.addTo(controllerBtn3);
+
+var controllerBtn4 = new ScrollMagic.Controller();
+var sceneBtn4 = new ScrollMagic.Scene({
+    triggerElement: '.btn4',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn4', 'show')
+.addTo(controllerBtn4);
+
+var controllerBtn5 = new ScrollMagic.Controller();
+var sceneBtn5 = new ScrollMagic.Scene({
+    triggerElement: '.btn5',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn5', 'show')
+.addTo(controllerBtn5);
+
+var controllerBtn6 = new ScrollMagic.Controller();
+var sceneBtn6 = new ScrollMagic.Scene({
+    triggerElement: '.btn6',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn6', 'show')
+.addTo(controllerBtn6);
+
+var controllerBtn7 = new ScrollMagic.Controller();
+var sceneBtn7 = new ScrollMagic.Scene({
+    triggerElement: '.btn7',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn7', 'show')
+.addTo(controllerBtn7);
+
+var controllerBtn8 = new ScrollMagic.Controller();
+var sceneBtn8 = new ScrollMagic.Scene({
+    triggerElement: '.btn8',
+    offset: -250,
+    duration: 700
+})
+.setClassToggle('.btn8', 'show')
+.addTo(controllerBtn8);
+
+var controllerBtn9 = new ScrollMagic.Controller();
+var sceneBtn9 = new ScrollMagic.Scene({
+    triggerElement: '.btn9',
+    offset: -450,
+    duration: 700
+})
+.setClassToggle('.btn9', 'show')
+.addTo(controllerBtn9);
+
+var controllerCard1 = new ScrollMagic.Controller();
+var card1 = new ScrollMagic.Scene({
+    triggerElement: '.card1',
+    offset: -350,
+    duration: 700
+})
+.setClassToggle('.card1', 'show')
+.addTo(controllerCard1);
+
+var controllerCard2 = new ScrollMagic.Controller();
+var card2 = new ScrollMagic.Scene({
+    triggerElement: '.card2',
+    offset: -350,
+    duration: 700
+})
+.setClassToggle('.card2', 'show')
+.addTo(controllerCard2);
+
+var controllerCard3 = new ScrollMagic.Controller();
+var card3 = new ScrollMagic.Scene({
+    triggerElement: '.card3',
+    offset: -350,
+    duration: 700
+})
+.setClassToggle('.card3', 'show')
+.addTo(controllerCard3);
+
+var controllerCard4 = new ScrollMagic.Controller();
+var card4 = new ScrollMagic.Scene({
+    triggerElement: '.card4',
+    offset: -350,
+    duration: 700
+})
+.setClassToggle('.card4', 'show')
+.addTo(controllerCard4);
+
+var controllerCard5 = new ScrollMagic.Controller();
+var card5 = new ScrollMagic.Scene({
+    triggerElement: '.card5',
+    offset: -350,
+    duration: 700
+})
+.setClassToggle('.card5', 'show')
+.addTo(controllerCard5);
+
+var controllerCard6 = new ScrollMagic.Controller();
+var card6 = new ScrollMagic.Scene({
+    triggerElement: '.card6',
+    offset: -350,
+    duration: 700
+})
+.setClassToggle('.card6', 'show')
+.addTo(controllerCard6);
