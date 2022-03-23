4bcb43ec0e20a7886225c00fa94b5f1475a8e043
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-03-23 09:16:52 -0400

    google analytics, scroll mag dur rem

--- js/scrollMagic.js
+++ js/scrollMagic.js
@@ -1,8 +1,7 @@
 var controllerBtn1 = new ScrollMagic.Controller();
 var btn1 = new ScrollMagic.Scene({
     triggerElement: '.btn1',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn1', 'show')
 .addTo(controllerBtn1);
@@ -10,8 +9,7 @@ var btn1 = new ScrollMagic.Scene({
 var controllerAboutImg = new ScrollMagic.Controller();
 var aboutImg = new ScrollMagic.Scene({
     triggerElement: '#aboutImg',
-    offset: 100,
-    duration: 500
+    offset: 100
 })
 .setClassToggle('#aboutImg', 'show')
 .addTo(controllerAboutImg);
@@ -19,8 +17,7 @@ var aboutImg = new ScrollMagic.Scene({
 var controllerBtn2 = new ScrollMagic.Controller();
 var btn2 = new ScrollMagic.Scene({
     triggerElement: '.btn2',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn2', 'show')
 .addTo(controllerBtn2);
@@ -28,8 +25,7 @@ var btn2 = new ScrollMagic.Scene({
 var controllerBtn3 = new ScrollMagic.Controller();
 var btn3 = new ScrollMagic.Scene({
     triggerElement: '.btn3',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn3', 'show')
 .addTo(controllerBtn3);
@@ -37,8 +33,7 @@ var btn3 = new ScrollMagic.Scene({
 var controllerBtn4 = new ScrollMagic.Controller();
 var btn4 = new ScrollMagic.Scene({
     triggerElement: '.btn4',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn4', 'show')
 .addTo(controllerBtn4);
@@ -46,8 +41,7 @@ var btn4 = new ScrollMagic.Scene({
 var controllerBtn5 = new ScrollMagic.Controller();
 var btn5 = new ScrollMagic.Scene({
     triggerElement: '.btn5',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn5', 'show')
 .addTo(controllerBtn5);
@@ -55,8 +49,7 @@ var btn5 = new ScrollMagic.Scene({
 var controllerBtn6 = new ScrollMagic.Controller();
 var btn6 = new ScrollMagic.Scene({
     triggerElement: '.btn6',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn6', 'show')
 .addTo(controllerBtn6);
@@ -64,8 +57,7 @@ var btn6 = new ScrollMagic.Scene({
 var controllerBtn7 = new ScrollMagic.Controller();
 var btn7 = new ScrollMagic.Scene({
     triggerElement: '.btn7',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn7', 'show')
 .addTo(controllerBtn7);
@@ -73,8 +65,7 @@ var btn7 = new ScrollMagic.Scene({
 var controllerBtn8 = new ScrollMagic.Controller();
 var btn8 = new ScrollMagic.Scene({
     triggerElement: '.btn8',
-    offset: -350,
-    duration: 700
+    offset: -350
 })
 .setClassToggle('.btn8', 'show')
 .addTo(controllerBtn8);
