4da747085a74091023b99e797c067aaf9607bca5
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-03-24 15:43:50 -0400

    footer js

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+const currentYear = new Date().getFullYear();
+const footer = document.getElementById('footer');
+const myFooter = `
+    <div class='text-center'>
+        <p>Copyright &#169;
+            ${currentYear}
+            <img id='footerIcon' class='d-none d-sm-inline' src="../favicon.ico" alt="LeNova, Inc. Icon.">
+            <span>LeNova, Inc.</span>
+        </p>
+    </div>
+`;
+
+const injectMyFooter = () => footer.innerHTML = myFooter;
