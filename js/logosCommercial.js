15f330ce350053f8bb78524f149a6d97e4f1580d
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-02 10:58:38 -0400

    logo js formatted

--- js/logosCommercial.js
+++ js/logosCommercial.js
@@ -1,65 +1,240 @@
 const logosCommercial = `
-    <a href="https://www.verizon.com/" target="_blank"
-    style="color:white;background-color:black"><span class='fst-italic'
-        style="color:red;font-size:3rem;">V</span>erizon</a>
-    <a href="https://www.borealisgroup.com/" target="_blank" class="boxShadowBig"
-    style="color:darkblue;border-radius:50%;border-top:4px solid steelblue;border-bottom:4px solid steelblue;">Borealis<br><span
-        class='textShadowBlack' style="color:white;font-size:1.25rem;">Chemicals</span></a>
-    <a href="https://www.genadyne.com/" target="_blank" class="boxShadowBig textShadowBlack"
-    style="background-color:indigo;color:white;border-radius:50% 0 50% 0;">GENADYNE<br><span
-        style="font-size:1.25rem;font-weight:normal">Medical
-        Devices</span></a>
-    <a href="https://flyingfood.com/" target="_blank"
-    style="color:navy;border-left:16px groove blue;border-top:8px groove blue;border-radius: 50% 0 0 0;">Flying
-    Food<br>Group</a>
-    <a href="https://www2.gerdau.com/" target="_blank" class="boxShadowBig"
-    style="color:navy;border-left:10px solid orange;border-right:10px solid purple;border-radius:50%;">GERDAU<br><span
-        class="textShadowBlack" style="color:white;font-size:1.25rem;">Steel Mill</span></a>
-    <a href="https://gelspice.com/" target="_blank" class="textShadowBlack"
-    style="color:darkgreen;border:8px double darkgreen;border-radius:50%;">Gel Spice<br><span
-        style="color:red;font-size:1.25rem;">Company</span></a>
-    <a href="https://crpindustries.com/" target="_blank" class="boxShadowBig textShadowBlack"
-    style="color:cornflowerblue;border:4px solid cornflowerblue">C<span style="color:blue">R</span><span
-        style="color:navy">P</span><br><span class='fst-italic' style="color:blue">Industries</span></a>
-    <a href="#" class="boxShadowBig"
-    style="color:black;border:4px solid black;border-radius:10px 100px / 120px;">Alliant<br>TechSystems</a>
-    <a href="https://www.bedminsterschool.org/" target="_blank" class='boxShadowBig textShadowBlack'
-    style="background-color:silver;color:navy;border-radius:50%;border-left:8px double navy;border-right:8px double navy;">Bedminster<br><span
-        class="text-white">School</span></a>
-    <a href="https://www.chatham-nj.org/" target="_blank"
-    style="color:white;background-color:navy;border-radius:50%;border-left:12px double white;border-top:12px double white;border-bottom:12px double white;">Chathams<br>School</a>
-    <a href="https://www.njstatelib.org/" target="_blank" class='boxShadowBig'
-    style="border-top:8px double navy;border-bottom:8px double navy;color:navy;background-color:silver;font-size:1rem;"><span
-        style="font-size:1.5rem;">N</span>ew <span style="font-size:1.5rem;">J</span>ersey<br><span
-        class="fst-italic">State Library</span></a>
-    <a href="https://princetonbar.com/" target="_blank"
-    style="color:black;background-color:darkturquoise;font-size:1rem;"><span
-        style="background-color:red;font-size:1.5rem;border-radius:50%;color:white;padding:0 0.5rem;;">P</span>rinceton
-    <span style="background-color:aqua;border-radius:50%;padding:0 0.25rem;">B</span>ar &<br><span
-        style="background-color:aqua;border-radius:50%;padding:0 0.25rem;">G</span>rill in Avalon</a>
-    <a href="https://www.choicehotels.com/quality-inn" target="_blank" class='textShadowBlack boxShadowBig'
-    style="line-height:1;color:darkgoldenrod;background-color:green;border-radius:4px;"><span
-        style="color:goldenrod;font-size:2rem;">Q</span>uality<br><span class="text-white fs-6">Inn &
-        Suites</span></a>
-    <a href="https://retrofitness.com/" target="_blank" class='fst-italic textShadowBlack'
-    style="font-size:1rem;background:rgb(50,50,50);color:white;border-top:12px solid yellow;border-bottom:12px solid orangered;border-radius: 50%;"><span
-        style="font-size:1.5rem;">R</span>etro<br><span style="font-size:1.5rem;">F</span>itness</a>
-    <a href="https://www.peacockinn.com/" target="_blank" class='boxShadowBig textShadowBlack'
-    style="color:dimgray;background-color:goldenrod;border-radius:50%;border:2px solid dimgray">Peacock<br><span
-        class="text-silver">Inn</span></a>
-    <a href="https://www.the-anchor.church/" target="_blank"
-    style="letter-spacing:-2px;line-height:1;font-size:1rem;color:darkblue;background-color:white;border-left:12px inset darkblue;border-top:12px inset darkblue;border-right:12px inset darkblue;border-bottom:12px inset darkblue;border-radius:0 0 50% 50%;">The
-    Anchor<br>Presbyterian<br>Church</a>
-    <a href="https://www.clearbrook-nj.com/" target="_blank"
-    style="font-size:1rem;color:cornflowerblue;background-color:white;border:2px solid limegreen;border-radius: 50% 20% / 10% 40%;">CLEARBROOK<br>Adult
-    Living</a>
-    <a href="http://www.motbc.org/" target="_blank"
-    style="font-size:1rem;color:white;background-color:steelblue;border:2px solid dimgray;">Mount
-    Olivet<br>Baptist Church</a>
-    <a href="https://www.princeton.edu/" target="_blank" class="boxShadowBig"
-    style="border-radius:0 0 50% 50%;color:black;background-color:orange;border-top: 8px solid black; border-left: 5px solid transparent;border-right: 5px solid transparent;border-bottom: 8px solid black;">Princeton<br>University</a>
-    <a href="https://nj1015.com/" target="_blank" class="boxShadowBig"
-    style="color:darkslategray;background-color:gold;border-left:10px solid red;border-right:10px solid red;">Radio<br>101.5</a>
+    <a href="https://www.verizon.com/" 
+        target="_blank" 
+        style="
+            color:white;
+            background-color:black
+        ">
+        <span class='fst-italic' style="color:red;font-size:3rem;">V</span>erizon
+    </a>
+
+    <a href="https://www.borealisgroup.com/" 
+        target="_blank" 
+        class="boxShadowBig"
+        style="
+            color:darkblue;
+            border-radius:50%;
+            border-top:4px solid steelblue;
+            border-bottom:4px solid steelblue;
+        ">
+        Borealis<br><span class='textShadowBlack' style="color:white;font-size:1.25rem;">Chemicals</span>
+    </a>
+
+    <a href="https://www.genadyne.com/" 
+        target="_blank" 
+        class="boxShadowBig textShadowBlack" 
+        style="
+            background-color:indigo;
+            color:white;
+            border-radius:50% 0 50% 0;
+        ">
+        GENADYNE<br><span style="font-size:1.25rem;font-weight:normal">Medical Devices</span>
+    </a>
+
+    <a href="https://flyingfood.com/" 
+    target="_blank" 
+    style="
+        color:navy;
+        border-left:16px groove blue;
+        border-top:8px groove blue;
+        border-radius: 50% 0 0 0;
+    ">
+    Flying Food<br>Group
+    </a>
+
+    <a href="https://www2.gerdau.com/" 
+        target="_blank" 
+        class="boxShadowBig" 
+        style="
+            color:navy;
+            border-left:10px solid orange;
+            border-right:10px solid purple;
+            border-radius:50%;
+        ">
+        GERDAU<br><span class="textShadowBlack" style="color:white;font-size:1.25rem;">Steel Mill</span>
+    </a>
+
+    <a href="https://gelspice.com/" 
+        target="_blank" 
+        class="textShadowBlack"
+        style="
+            color:darkgreen;
+            border:8px double darkgreen;
+            border-radius:50%;
+        ">
+        Gel Spice<br><span style="color:red;font-size:1.25rem;">Company</span>
+    </a>
+
+    <a href="https://crpindustries.com/" 
+        target="_blank" 
+        class="boxShadowBig textShadowBlack"
+        style="
+            color:cornflowerblue;
+            border:4px solid cornflowerblue
+        ">C<span style="color:blue">R</span><span style="color:navy">P</span><br><span class='fst-italic' style="color:blue">Industries</span>
+    </a>
+
+    <a href="#" 
+        class="boxShadowBig"
+        style="
+            color:black;
+            border:4px solid black;
+            border-radius:10px 100px / 120px;
+        ">
+        Alliant<br>TechSystems
+    </a>
+
+    <a href="https://www.bedminsterschool.org/" 
+        target="_blank" 
+        class='boxShadowBig textShadowBlack'
+        style="
+            background-color:silver;
+            color:navy;
+            border-radius:50%;
+            border-left:8px double navy;
+            border-right:8px double navy;
+        ">Bedminster<br><span
+        class="text-white">School</span>
+    </a>
+
+    <a href="https://www.chatham-nj.org/" 
+        target="_blank"
+        style="
+            color:white;
+            background-color:navy;
+            border-radius:50%;
+            border-left:12px double white;
+            border-top:12px double white;
+            border-bottom:12px double white;
+        ">Chathams<br>School
+    </a>
+
+    <a href="https://www.njstatelib.org/" 
+        target="_blank" 
+        class='boxShadowBig'
+        style="
+            border-top:8px double navy;
+            border-bottom:8px double navy;
+            color:navy;
+            background-color:silver;
+            font-size:1rem;
+        "><span style="font-size:1.5rem;">N</span>ew <span style="font-size:1.5rem;">J</span>ersey<br><span class="fst-italic">State Library</span>
+    </a>
+    
+    <a href="https://princetonbar.com/" 
+        target="_blank"
+        style="
+            color:black;
+            background-color:darkturquoise;
+            font-size:1rem;
+        ">
+        <span style="background-color:red;font-size:1.5rem;border-radius:50%;color:white;padding:0 0.5rem;;">P</span>rinceton<span style="background-color:aqua;border-radius:50%;padding:0 0.25rem;">B</span>ar &<br><span style="background-color:aqua;border-radius:50%;padding:0 0.25rem;">G</span>rill in Avalon
+    </a>
+
+    <a href="https://www.choicehotels.com/quality-inn" 
+        target="_blank" 
+        class='textShadowBlack boxShadowBig'
+        style="
+            line-height:1;
+            color:darkgoldenrod;
+            background-color:green;
+            border-radius:4px;
+        ">
+        <span style="color:goldenrod;font-size:2rem;">Q</span>uality<br><span class="text-white fs-6">Inn &Suites</span>
+    </a>
+
+    <a href="https://retrofitness.com/" 
+        target="_blank" 
+        class='fst-italic textShadowBlack'
+        style="
+            font-size:1rem;
+            background:rgb(50,50,50);
+            color:white;
+            border-top:12px solid yellow;
+            border-bottom:12px solid orangered;
+            border-radius: 50%;
+        ">
+        <span style="font-size:1.5rem;">R</span>etro<br><span style="font-size:1.5rem;">F</span>itness
+    </a>
+
+    <a href="https://www.peacockinn.com/" 
+        target="_blank" 
+        class='boxShadowBig textShadowBlack'
+        style="
+            color:dimgray;
+            background-color:goldenrod;
+            border-radius:50%;
+            border:2px solid dimgray
+        ">
+        Peacock<br><span class="text-silver">Inn</span>
+    </a>
+
+    <a href="https://www.the-anchor.church/" 
+        target="_blank"
+        style="
+            letter-spacing:-2px;
+            line-height:1;
+            font-size:1rem;
+            color:darkblue;
+            background-color:white;
+            border-left:12px inset darkblue;
+            border-top:12px inset darkblue;
+            border-right:12px inset darkblue;
+            border-bottom:12px inset darkblue;
+            border-radius:0 0 50% 50%;
+        ">
+        The Anchor<br>Presbyterian<br>Church
+    </a>
+
+    <a href="https://www.clearbrook-nj.com/" 
+        target="_blank"
+        style="
+            font-size:1rem;
+            color:cornflowerblue;
+            background-color:white;
+            border:2px solid limegreen;
+            border-radius: 50% 20% / 10% 40%;
+        ">
+        CLEARBROOK<br>Adult Living
+    </a>
+
+    <a href="http://www.motbc.org/" 
+        target="_blank"
+        style="
+            font-size:1rem;
+            color:white;
+            background-color:steelblue;
+            border:2px solid dimgray;
+        ">
+        Mount Olivet<br>Baptist Church
+    </a>
+
+    <a href="https://www.princeton.edu/" 
+        target="_blank" 
+        class="boxShadowBig"
+        style="
+            border-radius:0 0 50% 50%;
+            color:black;
+            background-color:orange;
+            border-top: 8px solid black; 
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-bottom: 8px solid black;
+        ">
+        Princeton<br>University
+    </a>
+
+    <a href="https://nj1015.com/" 
+        target="_blank" 
+        class="boxShadowBig"
+        style="
+            color:darkslategray;
+            background-color:gold;
+            border-left:10px solid red;
+            border-right:10px solid red;
+        ">
+        Radio<br>101.5
+    </a>
 `;
 
 const injectLogosCommercial = () => logosScroll.innerHTML += logosCommercial;
