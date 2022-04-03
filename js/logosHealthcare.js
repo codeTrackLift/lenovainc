722bb71a63886f30ff8bf419991034ede8c57696
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-03 08:52:24 -0400

    hcare logos aesthes

--- js/logosHealthcare.js
+++ js/logosHealthcare.js
@@ -6,8 +6,9 @@ const logosHealthcare = `
             color:dimgray;
             background-color:aquamarine;
             border-radius: 50% 20% / 10% 40%;
+            line-height: 1;
         ">
-        Aria<br><span class="text-white">Health</span>
+        Aria<br><span class="text-white" style='font-size:1rem'>Health</span>
     </a>
 
     <a href="https://www.hamiltonhealthcarecenter.org/" 
@@ -18,18 +19,24 @@ const logosHealthcare = `
             border-top:12px double steelblue;
             border-bottom:12px double darkkhaki;
             border-radius:50%;
-        ">Hamilton<br>HealthCare
+            line-height:1;
+        ">
+        Hamilton<br><span style='font-size:1.2rem'>HealthCare</span>
     </a>
 
     <a href="https://www.virtua.org/locations/hospital-virtua-our-lady-of-lourdes" 
         target="_blank"
-        style="background-color:steelblue;
+        class="boxShadowBig"
+        style="
+            background-color:steelblue;
             color:white;
             border-radius:25% 0 25% 0;
             border-left:12px groove khaki;
             border-right:12px groove khaki;
+            line-height:1;
+            font-size:1.25rem;
         ">
-        Our Lady of<br>Lourdes Hospital
+        <span style='font-weight:lighter;font-size:1rem;'>Our</span> Lady <span style='font-weight:lighter;font-size:1rem;'>of</span><br>Lourdes<br>Hospital
     </a>
 
     <a href="https://www.rwjbh.org/rwj-university-hospital-new-brunswick/" 
@@ -39,8 +46,9 @@ const logosHealthcare = `
             background-color:silver;
             color:steelblue;
             border-radius:4px;
+            line-height:1;
         ">
-        R<span style="font-size:1rem;">obert</span>W<span style="font-size:1rem;">ood</span>J<span style="font-size:1rem;">ohnson</span><br><span style="font-size: 1rem; background-color:gold;padding:0.5rem 1.5rem;margin:0 -1.5rem;">University Hospital</span>
+        R<span style="font-size:0.75rem;">obert</span>W<span style="font-size:0.75rem;">ood</span>J<span style="font-size:0.75rem;">ohnson</span><br><span style="font-size: 1rem; background-color:gold;padding:0.25rem;">University Hospital</span>
     </a>
 `;
 
