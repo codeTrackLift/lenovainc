1f732b2462902a68efc0b9140f15f8c748e2fa25
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-03 18:40:33 -0400

    footer icon rotate

--- js/footer.js
+++ js/footer.js
@@ -4,10 +4,11 @@ const myFooter = `
     <div class='text-center'>
         <p>Copyright &#169;
             ${currentYear}
-            <img id='footerIcon' class='d-none d-sm-inline' src="../pics/logos/lenovaIcon.ico" alt="LeNova, Inc. Icon.">
+            <img id='footerIcon' class='d-none d-sm-inline dropShadow' src="../pics/logos/lenovaIcon.ico" alt="LeNova, Inc. Icon.">
             <span>LeNova, Inc.</span>
         </p>
     </div>
 `;
 
 const injectMyFooter = () => footer.innerHTML = myFooter;
+
