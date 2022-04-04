7f3f04c39383670478825a49f4d247774fce5d01
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-04 17:50:45 -0400

    compliance logos

--- js/logosCompliance.js
+++ js/logosCompliance.js
@@ -1,88 +1,47 @@
 const logosCompliance = `
-    <a href="https://ispe.org/" 
-        target="_blank" 
-        class="textShadowBlack dropShadow"
-        style="
-            line-height: 1;
-            border-radius: 50%;
-            padding: 28px 14px;
-            color:dimgray;
-            font-size: 1.75rem;
-            box-shadow:none;
-            background: repeating-linear-gradient(
-                135deg,
-                transparent,
-                transparent 12.5px,
-                cornflowerblue 12.5px,
-                cornflowerblue 25px
-            );
-        ">
-        ISPE
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://ispe.org/" target="_blank">
+        <img src='../pics/logos/ispeLogo.png' alt='ISPE logo' class='dropShadow'>
+    </a><a href='#'></a>
+
+    <a href="https://www.iso.org/" target="_blank">
+        <img src='../pics/logos/isoLogoRed.png' alt='ISO logo' class='boxShadowBig'>
+    </a><a href='#'></a>
+
+    <a href="https://www.iccsafe.org/" target="_blank">
+        <img src='../pics/logos/imcLogo.jpg' alt='IMC logo' class='boxShadowBig'>
+    </a><a href='#'></a>
     
-    <a href="https://www.fda.gov/" 
-        target="_blank" 
-        class="boxShadowBig"
-        style="
-            line-height: 1;
-        ">
-        US FDA
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://www.fda.gov/" target="_blank">
+        <img src='../pics/logos/fdaLogo.jpg' alt='FDA logo' class='boxShadowBig'>
+    </a><a href='#'></a>
 
-    <a href="https://www.mfds.go.kr/eng/index.do" 
-        target="_blank" 
-        class="boxShadowBig"
-        style="
-            line-height: 1;
-        ">
-        Ministry of Food and Drug Safety
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://www.mfds.go.kr/eng/" target="_blank">
+        <img src='../pics/logos/mfdsLogo.png' alt='MFDS logo'>
+    </a><a href='#'></a>
 
-    <a href="https://www.ema.europa.eu/en" 
-        target="_blank" 
-        class="boxShadowBig"
-        style="
-            line-height: 1;
-        ">
-        EU MEA
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://www.ema.europa.eu/en" target="_blank">
+        <img src='../pics/logos/emaLogo.png' alt='EMA logo'>
+    </a><a href='#'></a>
 
-    <a href="https://www.canada.ca/en/" 
-        target="_blank" 
-        class="boxShadowBig"
-        style="
-            line-height: 1;
-        ">
-        Canada FDA
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://www.canada.ca/en/" target="_blank">
+        <img src='../pics/logos/healthCanadaLogo.png' alt='Canada logo'>
+    </a><a href='#'></a>
 
+    <a href="https://www.osha.gov/" target="_blank">
+        <img src='../pics/logos/oshaLogo.png' alt='OSHA logo'>
+    </a><a href='#'></a>
 
-    <a href="https://www.iso.org/home.html" 
-        target="_blank" 
-        class="boxShadowBig"
-        style="
-            line-height: 1;
-        ">
-        ISO
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://www.nfpa.org/" target="_blank">
+        <img src='../pics/logos/nfpaLogo.png' alt='NFPA logo'>
+    </a><a href='#'></a>
 
-    <a href="https://www.osha.gov/" 
-        target="_blank" 
-        class="boxShadowBig"
-        style="
-            line-height: 1;
-        ">
-        OSHA
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://www.iccsafe.org/" target="_blank">
+        <img src='../pics/logos/ibcLogo.jpg' alt='IBC logo' class='boxShadowBig'>
+    </a><a href='#'></a>
 
-    <a href="https://www.nfpa.org/" 
-        target="_blank" 
-        class="boxShadowBig"
-        style="
-            line-height: 1;
-        ">
-        NFPA
-    </a><a href='#' style='box-shadow:none;'></a>
+    <a href="https://www.ecfr.gov/" target="_blank">
+        <img src='../pics/logos/cfrLogo.jpg' alt='ECFR logo' class='boxShadowBig' style='border-radius:50%'>
+    </a><a href='#'></a>
 
 `;
 
